0847e3e0817fb39de801ef56faaa33e13671eb86
Erik Peterson <erikeh1@gmail.com>
2018-01-16 01:04:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ function multiply(a, b) {
 }
 function divide(a, b) {
   return a/b;
-  }
-  function inc(n) 
-  return ++n;
-  }
-  function 
+}
+function inc(n) 
+return ++n;
+}
+function dec(n) 
+return --n;
+}
